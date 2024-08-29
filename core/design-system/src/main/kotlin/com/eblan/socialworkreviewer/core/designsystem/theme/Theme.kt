343ea26858655cb84e204a4c71373cfb749cb04a
JackEblan <jackeblan115@gmail.com>
2024-08-29 16:29:03 +0800

    Change default theme to green and added splash

--- core/design-system/src/main/kotlin/com/eblan/socialworkreviewer/core/designsystem/theme/Theme.kt
+++ core/design-system/src/main/kotlin/com/eblan/socialworkreviewer/core/designsystem/theme/Theme.kt
@@ -34,64 +34,6 @@ import androidx.compose.ui.unit.dp
 
 @VisibleForTesting
 val LightDefaultColorScheme = lightColorScheme(
-    primary = Purple40,
-    onPrimary = Color.White,
-    primaryContainer = Purple90,
-    onPrimaryContainer = Purple10,
-    secondary = Orange40,
-    onSecondary = Color.White,
-    secondaryContainer = Orange90,
-    onSecondaryContainer = Orange10,
-    tertiary = Blue40,
-    onTertiary = Color.White,
-    tertiaryContainer = Blue90,
-    onTertiaryContainer = Blue10,
-    error = Red40,
-    onError = Color.White,
-    errorContainer = Red90,
-    onErrorContainer = Red10,
-    background = DarkPurpleGray99,
-    onBackground = DarkPurpleGray10,
-    surface = DarkPurpleGray99,
-    onSurface = DarkPurpleGray10,
-    surfaceVariant = PurpleGray90,
-    onSurfaceVariant = PurpleGray30,
-    inverseSurface = DarkPurpleGray20,
-    inverseOnSurface = DarkPurpleGray95,
-    outline = PurpleGray50,
-)
-
-@VisibleForTesting
-val DarkDefaultColorScheme = darkColorScheme(
-    primary = Purple80,
-    onPrimary = Purple20,
-    primaryContainer = Purple30,
-    onPrimaryContainer = Purple90,
-    secondary = Orange80,
-    onSecondary = Orange20,
-    secondaryContainer = Orange30,
-    onSecondaryContainer = Orange90,
-    tertiary = Blue80,
-    onTertiary = Blue20,
-    tertiaryContainer = Blue30,
-    onTertiaryContainer = Blue90,
-    error = Red80,
-    onError = Red20,
-    errorContainer = Red30,
-    onErrorContainer = Red90,
-    background = DarkPurpleGray10,
-    onBackground = DarkPurpleGray90,
-    surface = DarkPurpleGray10,
-    onSurface = DarkPurpleGray90,
-    surfaceVariant = PurpleGray30,
-    onSurfaceVariant = PurpleGray80,
-    inverseSurface = DarkPurpleGray90,
-    inverseOnSurface = DarkPurpleGray10,
-    outline = PurpleGray60,
-)
-
-@VisibleForTesting
-val LightAndroidColorScheme = lightColorScheme(
     primary = Green40,
     onPrimary = Color.White,
     primaryContainer = Green90,
@@ -120,7 +62,7 @@ val LightAndroidColorScheme = lightColorScheme(
 )
 
 @VisibleForTesting
-val DarkAndroidColorScheme = darkColorScheme(
+val DarkDefaultColorScheme = darkColorScheme(
     primary = Green80,
     onPrimary = Green20,
     primaryContainer = Green30,
@@ -148,7 +90,65 @@ val DarkAndroidColorScheme = darkColorScheme(
     outline = GreenGray60,
 )
 
-val LightAndroidBackgroundTheme = BackgroundTheme(color = DarkGreenGray95)
+@VisibleForTesting
+val LightAndroidColorScheme = lightColorScheme(
+    primary = Purple40,
+    onPrimary = Color.White,
+    primaryContainer = Purple90,
+    onPrimaryContainer = Purple10,
+    secondary = Orange40,
+    onSecondary = Color.White,
+    secondaryContainer = Orange90,
+    onSecondaryContainer = Orange10,
+    tertiary = Blue40,
+    onTertiary = Color.White,
+    tertiaryContainer = Blue90,
+    onTertiaryContainer = Blue10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = DarkPurpleGray99,
+    onBackground = DarkPurpleGray10,
+    surface = DarkPurpleGray99,
+    onSurface = DarkPurpleGray10,
+    surfaceVariant = PurpleGray90,
+    onSurfaceVariant = PurpleGray30,
+    inverseSurface = DarkPurpleGray20,
+    inverseOnSurface = DarkPurpleGray95,
+    outline = PurpleGray50,
+)
+
+@VisibleForTesting
+val DarkAndroidColorScheme = darkColorScheme(
+    primary = Purple80,
+    onPrimary = Purple20,
+    primaryContainer = Purple30,
+    onPrimaryContainer = Purple90,
+    secondary = Orange80,
+    onSecondary = Orange20,
+    secondaryContainer = Orange30,
+    onSecondaryContainer = Orange90,
+    tertiary = Blue80,
+    onTertiary = Blue20,
+    tertiaryContainer = Blue30,
+    onTertiaryContainer = Blue90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = DarkPurpleGray10,
+    onBackground = DarkPurpleGray90,
+    surface = DarkPurpleGray10,
+    onSurface = DarkPurpleGray90,
+    surfaceVariant = PurpleGray30,
+    onSurfaceVariant = PurpleGray80,
+    inverseSurface = DarkPurpleGray90,
+    inverseOnSurface = DarkPurpleGray10,
+    outline = PurpleGray60,
+)
+
+val LightAndroidBackgroundTheme = BackgroundTheme(color = DarkPurpleGray95)
 
 val DarkAndroidBackgroundTheme = BackgroundTheme(color = Color.Black)
 
@@ -162,17 +162,17 @@ val dynamicTopBarTitleGradientColors = GradientColors(
 
 val defaultTopBarTitleGradientColors = GradientColors(
     topBarTitleColorsDefault = listOf(
-        Color(0xFF1E88E5),
-        Color(0xFFD81B60),
-        Color(0xFF9C27B0),
+        Color(0xFF43A047),
+        Color(0xFF7CB342),
+        Color(0xFF039BE5),
     ),
 )
 
 val androidTopBarTitleGradientColors = GradientColors(
     topBarTitleColorsDefault = listOf(
-        Color(0xFFFDD835),
-        Color(0xFF00ACC1),
-        Color(0xFF4CAF50),
+        Color(0xFF8E24AA),
+        Color(0xFFD81B60),
+        Color(0xFF039BE5),
     ),
 )
 
