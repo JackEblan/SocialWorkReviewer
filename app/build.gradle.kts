d4af9cf4e76e42d004e986bc355bfe30e9e9db94
JackEblan <jackeblan115@gmail.com>
2024-10-05 07:19:29 +0800

    Updated to v1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
 
     defaultConfig {
         applicationId = "com.eblan.socialworkreviewer"
-        versionCode = 105
-        versionName = "1.0.5"
+        versionCode = 106
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
